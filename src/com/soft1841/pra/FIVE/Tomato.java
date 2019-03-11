b6fd079a48952fe58463d989d9d868810aa729da
tangmenghui <qq155109>
2019-03-11 13:37:10 +0800

    daytwo

---
+++ src/com/soft1841/pra/FIVE/Tomato.java
@@ -0,0 +1,30 @@
+package com.soft1841.pra.FIVE;
+
+import java.util.Scanner;
+
+public class Tomato {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("今天的西红柿单价（单价格式为“3.00””）:");
+        String dayPrice = sc.next();
+        if (dayPrice.length() == 4){
+            try {
+                String message = "西红柿：" +dayPrice +"元/500克";
+                String[] strArr = message.split(":");
+                String unitPriceDtr = strArr[2].substring(0,4);
+                double weight = 650;
+                double unitPriceDou = Double.parseDouble(unitPriceDtr);
+                System.out.println(message+",顾客买了"+weight+"克的西红柿，需支付"+(float) (weight/500 *unitPriceDou)+"元");
+            }catch (ArrayIndexOutOfBoundsException aiobe){
+                aiobe.printStackTrace();
+            }catch (Exception e){
+                e.printStackTrace();
+            }finally {
+                sc.close();
+                System.out.println("控制台输入对象被关闭。");
+            }
+        }else {
+            System.out.println("输入西红柿单价时小数点需保留两位有效数字（如：3.00）！");
+        }
+    }
+}
