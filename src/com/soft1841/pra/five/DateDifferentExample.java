c5b28763e904837df10da2733c3bd516e33cb9b3
tangmenghui <qq155109>
2019-03-19 07:18:49 +0800

    List的接口学习

--- src/com/soft1841/pra/five/DateDifferentExample.java
+++ src/com/soft1841/pra/five/DateDifferentExample.java
@@ -11,41 +11,40 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateDifferentExample {
-    public static void main (String[] args) {
-        //分别定义起止时间
-        String firstTime = "2019-3-15 12:30:58";
-        String thirdTime = "2019-3-18 5:40:35";
-        String forthTime = "2019-3-18 7:51:25";
-        String fifthTime = "2019-3-18 6:57:35";
+    public static void main(String[] args) {
+        String[] times = {"2019-03-16 20:31:58", "2019-03-17 12:08:34", "2019-03-17 21:35:26", "2019-03-17 21:59:07"};
+        String[] results = new String[times.length];
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date currentTime = new Date();
-        String secondTime = format.format(currentTime);
-        Date d1,d2,d3,d4,d5;
-        long diff1 = 0;
-        long diff2 = 0;
-        long diff3 = 0;
-        //通过SampleDateFormat的对象来把String类型的时间对象转化为Date类型的对象
+        System.out.println("现在时间：" + format.format(new Date()));
+        for (int i = 0; i < times.length; i++) {
+            results[i] = timeCal(times[i]);
+            System.out.println(times[i] + "  " + results[i]);
+        }
+    }
+
+    private static String timeCal(String time) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date d1;
+        long diff = 0;
         try {
-            d1 = format.parse(firstTime);
-            d2 = format.parse(secondTime);
-            d3 = format.parse(thirdTime);
-            d4= format.parse(forthTime);
-            d5 = format.parse(fifthTime);
-            //毫秒ms的差值
-            diff1 = d2.getTime() - d1.getTime();
-            diff2 = d2.getTime() - d3.getTime();
-            diff3 = d2.getTime() - d5.getTime();
-        }catch (ParseException e){
+            d1 = format.parse(time);
+            Date now = new Date();
+            diff = now.getTime() - d1.getTime();
+        } catch (ParseException e) {
             e.printStackTrace();
         }
-        //将毫秒分别换算为分、小时、天
-        long diffMinutes = diff3 / (1000*60);
-        long diffHours   = diff2 / (1000*60*60);
-        long diffDays    = diff1 / (1000*60*60*24);
-        System.out.println("现在时间："+secondTime);
-        System.out.println(firstTime+"   "+Math.abs(diffDays)+"天前");
-        System.out.println(thirdTime+"   "+Math.abs(diffHours)+"小时前");
-        System.out.println(fifthTime+"   "+Math.abs(diffMinutes)+"分前");
-        System.out.println(forthTime+"   " + " 刚刚");
+        long seconds = diff / 1000;
+        long minutes = seconds / 60;
+        long hours = minutes / 60;
+        long days = hours / 24;
+        if (seconds < 60) {
+            return "刚刚";
+        } else if (minutes < 60) {
+            return minutes + "分钟前";
+        } else if (hours < 24) {
+            return hours + "小时前";
+        } else {
+            return days + "天前";
+        }
     }
 }
---
+++ src/com/soft1841/pra/six/ListBookTest.java
@@ -0,0 +1,26 @@
+package com.soft1841.pra.six;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListBookTest {
+    public static void main(String[] args) {
+        List<Book> list = new ArrayList<>();
+        list.add(new Book(1,"计算机网络基础",22.45));
+        list.add(new Book(2,"传统文化与沟通交流",18.60));
+        list.add(new Book(3,"数据库技术及应用",45.37));
+        list.add(new Book(4,"思想道德修养与法律基础",25.36));
+        System.out.println(list);
+        list.add(2,new Book(5,"高数",35.76));
+        System.out.println(list);
+        list.remove(2);
+        System.out.println(list);
+        list.set(2,new Book(7,"高职英语",25.13));
+        System.out.println(list);
+        System.out.println(list.get(1));
+        list.add(new Book(8,"数据库技术及应用",10.12));
+        System.out.println(list);
+        System.out.println(list.indexOf("数据库技术及应用"));
+        System.out.println(list.lastIndexOf("数据库技术及应用"));
+    }
+}
