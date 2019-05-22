d7d2f6d9814d864173d9826243dcac30e0008209
tangmenghui <qq155109>
2019-05-23 00:25:34 +0800

    Java设计模式

---
+++ src/com/soft1841/pra/twelve/RealSubject.java
@@ -0,0 +1,8 @@
+package com.soft1841.pra.twelve;
+
+public class RealSubject implements Subject {
+    @Override
+    public void buyMac() {
+        System.out.println("买一台Mac");
+    }
+}
---
+++ src/com/soft1841/pra/twelve/Subject.java
@@ -0,0 +1,5 @@
+package com.soft1841.pra.twelve;
+
+public interface Subject {
+    public void  buyMac();
+}
